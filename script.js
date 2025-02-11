69425e66e3e6954fe61f7e8b515147cb4f146541
Shivam Panchal <142678499+Panchal-Shivam@users.noreply.github.com>
2025-02-11 23:35:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 function toggleNav(event) {
     event.stopPropagation();
     let sidebar = document.getElementById("mySidebar");
-    if (sidebar.style.width === "65%") {
-        sidebar.style.width = "0";
-    } else {
-        sidebar.style.width = "65%"; /* Expands Sidebar to 65% */
-    }
+    sidebar.style.width = (sidebar.style.width === "65%") ? "0" : "65%";
 }
 
-function closeNav(event) {
+document.addEventListener("click", function (event) {
     let sidebar = document.getElementById("mySidebar");
-    if (sidebar.style.width === "65%") {
+    let menuBtn = document.querySelector(".menu-btn");
+
+    // Close sidebar if clicked outside and it's open
+    if (sidebar.style.width === "65%" && !sidebar.contains(event.target) && event.target !== menuBtn) {
         sidebar.style.width = "0";
     }
-}
+});
